59c19d36899ea6c5aef52d5689189eb80088b9d1
MoonR1seN <asia.ostafiychuk@gmail.com>
2024-03-29 14:03:52 +0200

    Created display orders

--- popup.js
+++ popup.js
@@ -3,20 +3,23 @@ import config from "./config.json" with {type: "json"};
 
 const orderNumber = document.getElementById("order-number");
 const orderButton = document.getElementById("order-button");
-const orderContainer = document.getElementById("order-result");
+const orderContainer = document.getElementById("order-container");
 
 const regex = /^\d+$/;
+
+let orderNumberValue;
 
 orderNumber.addEventListener("change", (event) => {
     orderNumber.textContent = event.target.value;
 });
 
 orderButton.addEventListener("click", () => {
-    const orderNumberValue = orderNumber.value;
+    orderNumberValue = orderNumber.value;
 
     if (isValid(orderNumberValue)) {
         const url = createUrl(orderNumberValue);
         fetchOrderTrackingData(url);
+
     } else {
         orderContainer.innerHTML = "<span>No tracking data available.</span>";
     }
@@ -33,17 +36,30 @@ const isOrderFound = (data) => {
     return data.result.status !== 'error';
 }
 
-const insertDate = (data) => {
-    return data.result;
+const insertData = (data) => {
+    const trackingData = data.result?.tracking_data;
+    let htmlContent = `<p>№${orderNumberValue}</p>`;
+
+    if (data.result.tracking_step === 'parcel_received') {
+        htmlContent += '<p>Отримано</p>';
+    } else {
+        htmlContent += '<p>Скасовано</p>';
+    }
+
+    htmlContent += '<ul>';
+    trackingData.forEach(element => {
+        htmlContent += `<li>${element.name}</li><p>${element.date}</p>`;
+    });
+    htmlContent += '</ul>';
+
+    return htmlContent;
 }
 
 const fetchOrderTrackingData = (url) => {
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            orderContainer.innerHTML = `<span id="tracking-board">${(isOrderFound(data)) ? insertDate(data) : "Замовлення не знайдено"}</span>`;
-            console.log(data);
-
+            orderContainer.innerHTML = `<div id="order-result">${(isOrderFound(data))? insertData(data): "<p>Замовлення не знайдено</p>"}</div>`
         })
         .catch(console.error);
 }
