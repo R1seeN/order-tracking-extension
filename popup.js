82b1210c138dd26b77bd93b09916ceac3f9b9527
Anastasia Ostafiychuk <124509475+MoonR1seN@users.noreply.github.com>
2024-04-05 08:38:33 +0300

    Added css styles

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ import config from "./config.json" with {type: "json"};
 
 const orderNumber = document.getElementById("order-number");
 const orderButton = document.getElementById("order-button");
-const orderContainer = document.getElementById("order-container");
+let orderContainer = document.getElementById("order-container");
 
 const regex = /^\d+$/;
 
@@ -14,14 +14,12 @@ orderNumber.addEventListener("change", (event) => {
 });
 
 orderButton.addEventListener("click", () => {
+
     orderNumberValue = orderNumber.value;
 
     if (isValid(orderNumberValue)) {
         const url = createUrl(orderNumberValue);
         fetchOrderTrackingData(url);
-
-    } else {
-        orderContainer.innerHTML = "<span>No tracking data available.</span>";
     }
 });
 
@@ -37,18 +35,24 @@ const isOrderFound = (data) => {
 }
 
 const insertData = (data) => {
+
     const trackingData = data.result?.tracking_data;
-    let htmlContent = `<p>№${orderNumberValue}</p>`;
+    let htmlContent = `<p class="result__number">№${orderNumberValue}</p>`;
 
     if (data.result.tracking_step === 'parcel_received') {
-        htmlContent += '<p>Отримано</p>';
+        htmlContent += '<div class="result__success">' +
+            '<p class="result__success__description">Отримано</p>' +
+            '</div>';
+
     } else {
-        htmlContent += '<p>Скасовано</p>';
+        htmlContent += '<div class="result__cancelled">' +
+            '<p class="result__cancelled__description">Скасоване</p>' +
+            '</div>';
     }
 
-    htmlContent += '<ul>';
+    htmlContent += '<ul class="result__list">';
     trackingData.forEach(element => {
-        htmlContent += `<li>${element.name}</li><p>${element.date}</p>`;
+        htmlContent += `<li class=${(element.is_active)? "result__list__active" : "result__list__disabled"}>${element.name}</li><p class="list__date">${element.date}</p>`;
     });
     htmlContent += '</ul>';
 
@@ -59,8 +63,14 @@ const fetchOrderTrackingData = (url) => {
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            orderContainer.innerHTML = `<div id="order-result">${(isOrderFound(data))? insertData(data): "<p>Замовлення не знайдено</p>"}</div>`
+            console.log(data)
+            orderContainer.innerHTML = `<div id="order-result">${(isOrderFound(data))? insertData(data): "<p class='result__notfound'>Замовлення не знайдено</p>"}</div>`
+
         })
         .catch(console.error);
+}
+
+const setToLocalStorage = () => {
+    localStorage.setItem(orderNumberValue, 'first');
 }
 
