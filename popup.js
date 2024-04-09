9b16f99b7d29305caa657e400666fb402029e58a
Anastasia Ostafiychuk <124509475+MoonR1seN@users.noreply.github.com>
2024-04-09 19:37:03 +0300

    Added local storage to app

--- popup.js
+++ popup.js
@@ -4,10 +4,34 @@ import config from "./config.json" with {type: "json"};
 const orderNumber = document.getElementById("order-number");
 const orderButton = document.getElementById("order-button");
 let orderContainer = document.getElementById("order-container");
+let orderDataList = document.getElementById("order");
 
 const regex = /^\d+$/;
 
 let orderNumberValue;
+
+const getItemLocalStorage = () => {
+    return localStorage.getItem('request-data') !== null ? localStorage.getItem('request-data').split(" ") : []
+}
+
+const insertDateFromLocalStorage = (itemLocalStorage) => {
+
+    for (const itemLocalStorageElement of itemLocalStorage) {
+        console.log(itemLocalStorageElement);
+
+        let option = document.createElement("option");
+
+        option.value = itemLocalStorageElement;
+        option.textContent = itemLocalStorageElement;
+
+        orderDataList.appendChild(option);
+    }
+}
+
+const itemLocalStorage = getItemLocalStorage();
+insertDateFromLocalStorage(itemLocalStorage);
+
+
 
 orderNumber.addEventListener("change", (event) => {
     orderNumber.textContent = event.target.value;
@@ -21,6 +45,7 @@ orderButton.addEventListener("click", () => {
         const url = createUrl(orderNumberValue);
         fetchOrderTrackingData(url);
     }
+
 });
 
 const isValid = (orderNumberValue) => regex.test(orderNumberValue);
@@ -63,20 +88,20 @@ const fetchOrderTrackingData = (url) => {
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            //setToLocalStorage(data);
+            setToLocalStorage();
             orderContainer.innerHTML = `<div id="order-result">${(isOrderFound(data))? insertData(data): "<p class='result__notfound'>Замовлення не знайдено</p>"}</div>`
 
         })
         .catch(console.error);
 }
 
-const setToLocalStorage = (data) => {
-    const requestDataJson = JSON.parse(data);
-    const requestData = requestDataJson !== null? requestDataJson : {};
-
-    requestData[orderNumberValue] = data;
-
-
+const setToLocalStorage = () => {
+    const requestDataJson = localStorage.getItem('request-data');
+    // console.log(requestDataJson);
+    let requestData = requestDataJson !== null ? JSON.parse(requestDataJson) : "";
+    if (!requestData.includes(orderNumberValue)) {
+        requestData += orderNumberValue + " ";
+    }
     localStorage.setItem('request-data', JSON.stringify(requestData));
 }
 
